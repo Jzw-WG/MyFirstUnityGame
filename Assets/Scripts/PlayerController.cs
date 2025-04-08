ddf86ebfe563840928d81127e647c70125d69b99
Jzw <qq710327563@126.com>
2025-04-09 00:43:36 +0800

    角色移动
    增加场景，角色移动，移动动画

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(0f, 0f, 0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(moveInput);
+
+        // 消除斜向移动速度变快的问题
+        moveInput.Normalize();
+        // * Time.deltaTime 消除帧率带来的速度差
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+
+        if (moveInput != Vector3.zero) {
+            anim.SetBool("isMoving", true);
+        } else {
+            anim.SetBool("isMoving", false);
+        }
+    }
+}
